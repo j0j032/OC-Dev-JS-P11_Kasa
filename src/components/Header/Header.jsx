6f5701442e767234ac10056de7e44c4d42a6b741
Jordan <100964858+j0j032@users.noreply.github.com>
2022-09-08 13:02:17 +0200

    🏗 setup architecture, clean CRA, import assets

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className={"header__container"}>
+            <img src={"./fullLogo.svg"} alt={"Kasa, location d'appartement entre particulier"}/>
+        </div>
+    );
+};
+
+export default Header;
