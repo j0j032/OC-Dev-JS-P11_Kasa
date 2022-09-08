09b9736408030b4958df76c1a57bd7232bf4b2b0
Jordan <100964858+j0j032@users.noreply.github.com>
2022-09-08 13:42:04 +0200

    🚧 set logo & navLinks (no style yet)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
     return (
         <div className={"header__container"}>
-            <img src={"./fullLogo.svg"} alt={"Kasa, location d'appartement entre particulier"}/>
+            <img className={"header__logo"} src={"./fullLogo.svg"} alt={"Kasa, location d'appartement entre particulier"}/>
+            <nav className={"header__nav"}>
+                <ul>
+                    <NavLink to={"/"} className={(nav) =>(nav.isActive ? "header__nav-active" : "")}>
+                        <li>Accueil</li>
+                    </NavLink>
+                    <NavLink to={"/about"} className={(nav) =>(nav.isActive ? "header__nav-active" : "")}>
+                        <li>A propos</li>
+                    </NavLink>
+                </ul>
+            </nav>
         </div>
     );
 };
