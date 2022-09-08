6b884d927be58b76e38fc4ae2bc0cbae32e31b2f
Jordan <100964858+j0j032@users.noreply.github.com>
2022-09-08 13:27:06 +0200

    🚚 set main routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import Header from "./components/Header/Header";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Error404 from "./pages/Error404";
 
 
 function App() {
   return (
-    <div>
-        <Header/>
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route path={'/'} element={<Home/>}/>
+            <Route path={'/about'} element={<About/>}/>
+            <Route path={'/*'} element={<Error404/>}/>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
