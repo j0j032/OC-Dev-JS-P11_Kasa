aee4a762a452e2f06670643728269e3bcd780993
Jordan <100964858+j0j032@users.noreply.github.com>
2022-09-09 21:02:29 +0200

    🚧💄 homepage style done without responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import {NavLink} from "react-router-dom";
+import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 const Header = () => {
-    return (
-        <div className={"header__container"}>
-            <img className={"header__logo"} src={"./fullLogo.svg"} alt={"Kasa, location d'appartement entre particulier"}/>
-            <nav>
-                <ul className={"header__nav"}>
-                    <NavLink to={"/"} className={(nav) =>(nav.isActive ? "header__nav-link--active" : "header__nav-link")}>
-                        <li>Accueil</li>
-                    </NavLink>
-                    <NavLink to={"/about"} className={(nav) =>(nav.isActive ? "header__nav-link--active" : "header__nav-link")}>
-                        <li>A propos</li>
-                    </NavLink>
-                </ul>
-            </nav>
-        </div>
-    );
-};
+	return (
+		<header className={'header__container'}>
+			<img className={'header__logo'} src={'./fullLogo.svg'}
+				 alt={'Kasa, location d\'appartement entre particulier'}/>
+			<nav>
+				<ul className={'header__nav'}>
+					<NavLink to={'/'}
+							 className={(nav) => (nav.isActive ? 'header__nav-link--active' : 'header__nav-link')}>
+						<li>Accueil</li>
+					</NavLink>
+					<NavLink to={'/about'}
+							 className={(nav) => (nav.isActive ? 'header__nav-link--active' : 'header__nav-link')}>
+						<li>A propos</li>
+					</NavLink>
+				</ul>
+			</nav>
+		</header>
+	)
+}
 
-export default Header;
+export default Header
