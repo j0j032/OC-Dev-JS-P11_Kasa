ef067327d3943e1eb2b4a13e55aef3d830b394e9
Jordan <100964858+j0j032@users.noreply.github.com>
2022-09-11 22:49:53 +0200

    🚧 infos are displaying (without any style)

--- src/components/lodgingInfos/LodgingInfos.jsx
+++ src/components/lodgingInfos/LodgingInfos.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import Rating from '../rating/Rating'
 
 const LodgingInfos = ({lodging}) => {
-	const {id, title, location, rating /* host: {name, picture}*/} = lodging
+	const {id, title, location, rating, host: {name, picture}} = lodging
 	
 	return (
 		<div>
 			<h1>{title}</h1>
 			<p>{location}</p>
-			{/*{
+			{
 				lodging.tags.map((tag) => <span key={`${id}-${tag}`}>{`${tag} `}</span>)
 			}
 			<p>{name}</p>
-			<img src={picture} alt={name}/>*/}
+			<img src={picture} alt={name}/>
 			<Rating rating={rating}/>
 		
 		</div>
